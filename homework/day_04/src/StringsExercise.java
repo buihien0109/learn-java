18c7eb2e10ad046344b8a73b5ddfbd582fbbcb05
buihien0109 <buihien01091997@gmail.com>
2021-12-24 11:41:12 +0700

    update

--- homework/day_04/src/StringsExercise.java
+++ homework/day_04/src/StringsExercise.java
@@ -4,6 +4,8 @@ public class StringsExercise {
     private static Scanner scanner = new Scanner(System.in);
     private static String[] strings;
 
+    // Nhập vào số lượng phần tử
+    // Nhập vào từng phần tử của mảng
     public static void input() {
         System.out.println("1. Nhập vào số lượng phần tử : ");
         int numbers = Integer.parseInt(scanner.nextLine());
@@ -15,6 +17,7 @@ public class StringsExercise {
         }
     }
 
+    // In các giá trị của mảng
     public static void print() {
         System.out.println("2. In mảng");
         for (int i = 0; i < strings.length ; i++) {
@@ -22,6 +25,7 @@ public class StringsExercise {
         }
     }
 
+    // Đếm số lượng chuỗi bất kỳ có trong mảng
     public static void countWord(String str) {
         int count = 0;
         for (int i = 0; i < strings.length ; i++) {
@@ -30,6 +34,14 @@ public class StringsExercise {
             }
         }
         System.out.println("3. Phần tử \"" + str + "\" xuất hiện " + count + " lần trong mảng.");
+    }
+
+    // Lấy vị trí xuất hiện của phần tử trong mảng
+    public static void printPositionWord() {
+        System.out.println("4. Nhập vào phần tử muốn kiểm tra vị trí: ");
+        String str = scanner.nextLine();
+
+        getPositionWord(str);
     }
 
     public static void getPositionWord(String str) {
@@ -45,13 +57,4 @@ public class StringsExercise {
             System.out.println("Phần tử " + str + " không xuất hiện trong mảng");
         }
     }
-
-    public static void printPositionWord() {
-        System.out.println("4. Nhập vào phần tử muốn kiểm tra vị trí: ");
-        String str = scanner.nextLine();
-
-        getPositionWord(str);
-    }
-
-
 }
